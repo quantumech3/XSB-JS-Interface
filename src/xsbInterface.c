d3d1e443bf1ae875308994a39d8412d6e1461d49
DESKTOP-JB24B0S\Scott Burgert <quantumech3@gmail.com>
2020-01-21 22:17:06 -0500

    Created C wrapper for XSB C Interface

--- src/xsbInterface.c
+++ src/xsbInterface.c
@@ -2,9 +2,73 @@
  * Summary:
  * 		This component contains XSB C Interface wrapper methods that are exported to JS during Emscripten's build phase
  * 
- * Overview:
- *		TODO: <INSERT OVERVIEW OF METHODS HERE AS YOU WRITE CODE>
  * 
  * Credits: Developed by Scott Burgert & Paul Fodor in 2020
  */
 
+#include "xsbInterface.h"
+
+/**
+ * Streight call to xsb_init_string()
+ */
+int _xsb_init_string(char* path)
+{
+	return xsb_init_string(path);
+}
+
+/**
+ * Streight call to xsb_close()
+ */
+int _xsb_close()
+{
+	return xsb_close();
+}
+
+/**
+ * Streight call to xsb_command_string()
+ */
+ int _xsb_command_string(char* command)
+ {
+	 return xsb_command_string(command);
+ }
+
+/**
+ * Streight call to _xsb_query_string_string_b()
+ */
+int _xsb_query_string_string_b(char* a, char* b, int c, int* d, char* e)
+{
+	return xsb_query_string_string_b(a, b, c, d, e);
+}
+
+/**
+ * Streight call to _xsb_next_string_b()
+ */
+int _xsb_next_string_b(char* a, int b, int* c, char* d)
+{
+	return xsb_next_string_b(a, b, c, d);
+}
+
+/**
+ * Streight call to _xsb_close_query()
+ */
+int _xsb_close_query()
+{
+	return xsb_close_query();
+}
+
+/**
+ * Streight call to _xsb_get_init_error_message()
+ */
+char* _xsb_get_init_error_message()
+{
+	return xsb_get_init_error_message();
+}
+
+/**
+ * Streight call to _xsb_get_error_message()
+ */
+EMSCRIPTEN_KEEPALIVE
+char* _xsb_get_error_message()
+{
+	return xsb_get_error_message();
+}
--- src/xsbInterface.h
+++ src/xsbInterface.h
@@ -5,5 +5,30 @@
  * Credits: Developed by Scott Burgert & Paul Fodor in 2020
  */
 
+#include <stdio.h>
 #include <cinterf.h>
 #include <emscripten.h>
+
+EMSCRIPTEN_KEEPALIVE
+int _xsb_command_string(char*); //
+
+EMSCRIPTEN_KEEPALIVE
+int _xsb_query_string_string_b(char*,char*,int,int*,char*); //
+
+EMSCRIPTEN_KEEPALIVE
+int _xsb_next_string_b(char*,int,int*,char*); //
+
+EMSCRIPTEN_KEEPALIVE
+int _xsb_close_query(); //
+
+EMSCRIPTEN_KEEPALIVE
+int _xsb_close(); // 
+
+EMSCRIPTEN_KEEPALIVE
+int _xsb_init_string(char*); //
+
+EMSCRIPTEN_KEEPALIVE
+char* _xsb_get_init_error_message(); // 
+
+EMSCRIPTEN_KEEPALIVE
+char* _xsb_get_error_message();
