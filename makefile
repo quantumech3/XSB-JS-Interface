1545849f1ada76ba8d8817244e4b4c52bd7ba676
DESKTOP-JB24B0S\Scott Burgert <quantumech3@gmail.com>
2020-01-21 21:33:31 -0500

    Created build system

--- makefile
+++ makefile
@@ -7,5 +7,14 @@
 #
 # Credits: Developed by Scott Burgert & Paul Fodor in 2020
 
+build:
+	# Execute Emscripten build command
+	emcc ./src/xsbInterface.c ./src/config/saved.o/xsb.o -o ./out/xsbInterface.js -Oz --preload-file "./src/xsbDir/@/" -I ./src/emu/ -I ./src/config/ -s USE_PTHREADS=1 -s TOTAL_MEMORY=1024MB
+	
+	# Append pre.js and post.js to beginning and end of xsbInterface.js
+	cp ./out/xsbInterface.js ./out/xsbInterface.temp.js
+	cat ./src/pre.js ./out/xsbInterface.temp.js ./src/post.js > ./out/xsbInterface.js
+	rm ./out/xsbInterface.temp.js
+
 run:
-	emrun .
+	emrun ./out/
--- src/pre.js
+++ src/pre.js
@@ -1,12 +1,14 @@
 /**
- * Summary:
- * 		This component contains all code in the JS component of the XSB-JS-Interface that is executed before the initialization of the Emscripten Runtime.
- * 		such as XSB.Event methods and Emscripten output handling overrides
- * 
- * Overview:
- * 		* XSB.Events: Structure containing various XSB Interpreter callbacks
- * 			* void onOutput(String command, Bool isError): This method is called when XSB would normally output to a terminal. 
- * 														   onOutput() overrides Emscripten's stdout and stderr handling
- * 
- * Credits: Developed by Scott Burgert & Paul Fodor in 2020
- */
+* 
+* Summary:
+* 		This component contains all code in the JS component of the XSB-JS-Interface that is executed before the initialization of the Emscripten Runtime.
+* 		such as XSB.Event methods and Emscripten output handling overrides
+* 
+* Overview:
+* 		* XSB.Events: Structure containing various XSB Interpreter callbacks
+* 			* void onOutput(String output, Bool isError): This method is called when XSB would normally output to a terminal. 
+* 														   onOutput() overrides Emscripten's stdout and stderr handling
+* 
+* Credits: Developed by Scott Burgert & Paul Fodor in 2020
+*/
+
